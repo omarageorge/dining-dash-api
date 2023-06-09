18d42a4fd621923f380400065702dcf775029dc9
George Omara <georgeomara8@gmail.com>
2023-06-09 14:56:55 +0300

    Database models

---
+++ models/Restaurant.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+const restaurantSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  location: { type: String, required: true },
+  imageSrc: { type: String },
+  cuisine: { type: String },
+  menu: [
+    {
+      name: { type: String, required: true },
+      description: { type: String, required: true },
+      ingredients: { type: String, required: true },
+      price: { type: String, required: true },
+    },
+  ],
+});
+
+const Restaurant = mongoose.model('Restaurant', restaurantSchema);
+
+export default Restaurant;
---
+++ models/User.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+userSchema.pre('save', async function (next) {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+    this.password = hashedPassword;
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
