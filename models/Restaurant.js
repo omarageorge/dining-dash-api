fdd0303ad5310ecb4dc0bdefc90d98bc3df48f29
George Omara <georgeomara8@gmail.com>
2023-06-09 16:54:56 +0300

    Added timestamp

--- models/Restaurant.js
+++ models/Restaurant.js
@@ -1,19 +1,24 @@
 import mongoose from 'mongoose';
 
-const restaurantSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  location: { type: String, required: true },
-  imageSrc: { type: String },
-  cuisine: { type: String },
-  menu: [
-    {
-      name: { type: String, required: true },
-      description: { type: String, required: true },
-      ingredients: { type: String, required: true },
-      price: { type: String, required: true },
-    },
-  ],
-});
+const restaurantSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    location: { type: String, required: true },
+    imageSrc: { type: String },
+    cuisine: { type: String },
+    menu: [
+      {
+        name: { type: String, required: true },
+        description: { type: String, required: true },
+        ingredients: { type: String, required: true },
+        price: { type: String, required: true },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const Restaurant = mongoose.model('Restaurant', restaurantSchema);
 
