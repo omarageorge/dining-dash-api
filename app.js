7798f04d0f0803cde172452d13bd896ecda072c1
George Omara <georgeomara8@gmail.com>
2023-06-09 16:53:02 +0300

    Configured middleware and routes

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
 import express from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
+import helmet from 'helmet';
 import dotenv from 'dotenv';
+import connectDB from './config/database.js';
+import errorHandler from './middleware/error.js';
+import authRouter from './routes/authRoutes.js';
+import restaurantRouter from './routes/restaurantRoutes.js';
 
 const app = express();
 
-/* Configs */
-dotenv.config();
+/* Init */
+dotenv.config(); // Load environment variables
+connectDB(); // Connect to MongoDB
 
 /* Middleware */
 app.use(morgan('dev'));
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('assets', express.static('public'));
+app.use(errorHandler);
 
-app.get('/', (req, res) => {
-  res.send('Server: OK');
-});
+app.use('/api/v1/auth', authRouter); // Auth Endpoints
+app.use('/api/v1/restaurants', restaurantRouter); // Restaurant Endpoints
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
