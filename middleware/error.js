c3ba09dbf73e96b622940c43ce37543f54490b48
George Omara <georgeomara8@gmail.com>
2023-06-09 16:52:21 +0300

    Error handler

---
+++ middleware/error.js
@@ -0,0 +1,9 @@
+const errorHandler = (err, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+  });
+};
+
+export default errorHandler;
