9f20149f6e626609df4878c1dc0b99f0254a2949
George Omara <georgeomara8@gmail.com>
2023-06-09 17:31:20 +0300

    Made restaurant name field unique

--- models/Restaurant.js
+++ models/Restaurant.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const restaurantSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
+    name: { type: String, required: true, unique: true },
     location: { type: String, required: true },
     imageSrc: { type: String },
     cuisine: { type: String },
