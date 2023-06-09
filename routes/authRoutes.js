53ca57d40e1ef4badc944dc49b439bb078a35553
George Omara <georgeomara8@gmail.com>
2023-06-09 17:30:40 +0300

    Added request method to comments

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,14 +4,14 @@ import { loginUser, registerUser } from '../controllers/authController.js';
 const router = Router();
 
 /* 
-    @route  /api/v1/auth/signup
+    @route  POST /api/v1/auth/signup
     @desc   Register new user
     @access Public
 */
 router.post('/signup', registerUser);
 
 /* 
-    @route  /api/v1/auth/login
+    @route  POST /api/v1/auth/login
     @desc   Login user
     @access Public
 */
