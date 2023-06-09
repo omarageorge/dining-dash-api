c9d1a4d3d5a48b807829d91c63a470ed0a38c030
George Omara <georgeomara8@gmail.com>
2023-06-09 19:41:06 +0300

    replaced assets with static

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(cors());
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('assets', express.static('public'));
+app.use('/static', express.static('public'));
 
 app.use('/api/v1/auth', authRouter); // Auth Endpoints
 app.use('/api/v1/restaurants', restaurantRouter); // Restaurant Endpoints
