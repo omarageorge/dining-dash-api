d0f2902bdbbd4e35e044a84efa54d1113417240f
George Omara <georgeomara8@gmail.com>
2023-06-09 17:31:54 +0300

    Moved Error handler to the bottom

--- app.js
+++ app.js
@@ -21,10 +21,11 @@ app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('assets', express.static('public'));
-app.use(errorHandler);
 
 app.use('/api/v1/auth', authRouter); // Auth Endpoints
 app.use('/api/v1/restaurants', restaurantRouter); // Restaurant Endpoints
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
